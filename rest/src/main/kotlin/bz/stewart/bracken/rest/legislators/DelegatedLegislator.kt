d416d1d8909315cb44e783ededbe1ab8754f771a
abigpotostew <stew.bracken@gmail.com>
2017-06-05 23:56:31 -0700

    fully mapped db legislator to public. untested

--- rest/src/main/kotlin/bz/stewart/bracken/rest/legislators/DelegatedLegislator.kt
+++ rest/src/main/kotlin/bz/stewart/bracken/rest/legislators/DelegatedLegislator.kt
@@ -1,11 +1,11 @@
 package bz.stewart.bracken.rest.legislators
 
-import bz.stewart.bracken.db.leglislators.data.*
-import bz.stewart.bracken.shared.DateUtils
+import bz.stewart.bracken.db.leglislators.data.LegislatorData
+import bz.stewart.bracken.db.leglislators.data.TermData
+import bz.stewart.bracken.db.leglislators.data.emptyTermDate
 import bz.stewart.bracken.shared.data.defaultPartyTypeMatcher
 import bz.stewart.bracken.shared.data.defaultRoleTypeMatcher
 import bz.stewart.bracken.shared.data.party.Party
-import bz.stewart.bracken.shared.data.person.Legislator
 import bz.stewart.bracken.shared.data.person.LegislatorRole
 import bz.stewart.bracken.shared.data.person.Person
 
@@ -15,7 +15,7 @@ import bz.stewart.bracken.shared.data.person.Person
 class DelegatedLegislator (private val p:LegislatorData):Person{
 
    private val roles :List<TermData> = p.terms ?: emptyList()
-   private val maxRole :TermData = roles.maxBy { it.end?.time ?: -1 } ?: emptyTermDate() //maybe null
+   private val currentRole:TermData = roles.maxBy { it.end?.time ?: -1 } ?: emptyTermDate() //maybe null
 
    private val DEFAULT_NO_NAME = ""
    override fun getBioguideId(): String {
@@ -43,26 +43,26 @@ class DelegatedLegislator (private val p:LegislatorData):Person{
    }
 
    override fun getParty(): Party {
-      return defaultPartyTypeMatcher(maxRole.party ?: "")
+      return defaultPartyTypeMatcher(currentRole.party ?: "")
    }
 
    override fun getRole(): LegislatorRole {
-      return defaultRoleTypeMatcher(maxRole.type ?: "")
+      return defaultRoleTypeMatcher(currentRole.type ?: "")
    }
 
    override fun getPhoneNumber(): String {
-      return p.getPhoneNumber()
+      return currentRole.phone ?: ""
    }
 
    override fun getWebsite(): String? {
-      return p.getWebsite()
+      return currentRole.url
    }
 
    override fun getState(): String {
-      return p.getState()
+      return currentRole.state ?: ""
    }
 
    override fun getTwitter(): String {
-      return p.getTwitter()
+      return p.social?.twitter ?: ""
    }
 }
