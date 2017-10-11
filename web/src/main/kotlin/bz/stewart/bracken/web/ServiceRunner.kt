d577306e5950cd9dd8fd3ad405d4f7e861b0597f
Stewart <stew.bracken@gmail.com>
2017-10-10 18:59:36 -0700

    static css

--- web/src/main/kotlin/bz/stewart/bracken/web/ServiceRunner.kt
+++ web/src/main/kotlin/bz/stewart/bracken/web/ServiceRunner.kt
@@ -4,6 +4,7 @@ import spark.Spark
 
 class ServiceRunner {
     fun run(){
+        Spark.staticFileLocation("/static")
         Spark.get("/hello") { req, res -> "Hello World" }
         Spark.get("/bill/:id") {
             req, res -> WebsiteSkeleton(BillWebView(req.params("id"))).render()
--- web/src/main/kotlin/bz/stewart/bracken/web/WebsiteSkeleton.kt
+++ web/src/main/kotlin/bz/stewart/bracken/web/WebsiteSkeleton.kt
@@ -4,6 +4,7 @@ import kotlinx.html.body
 import kotlinx.html.head
 import kotlinx.html.html
 import kotlinx.html.lang
+import kotlinx.html.link
 import kotlinx.html.meta
 import kotlinx.html.stream.createHTML
 import kotlinx.html.style
@@ -20,6 +21,12 @@ class WebsiteSkeleton(private val content:ViewTemplate) : WebView {
                     name="viewport"
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 }
+                link(rel = "stylesheet", href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"){
+                    this.attributes.put("integrity","sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ")
+                    this.attributes.put("crossorigin", "anonymous")
+                }
+                link ( rel="stylesheet", href = "/mainstyle.css")
+
                 title {
                     +"title"
                 }
