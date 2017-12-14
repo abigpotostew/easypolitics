6359e14ffb7a0ece48f5653c16bad972a246a58d
stew.bracken <stew.bracken@workday.com>
2017-12-13 23:39:29 -0800

    test db from environment properties

--- rest/src/main/kotlin/bz/stewart/bracken/rest/service/SingleBillService.kt
+++ rest/src/main/kotlin/bz/stewart/bracken/rest/service/SingleBillService.kt
@@ -13,7 +13,7 @@ class SingleBillService : RouteService<QueryResult> {
         if (billId==null){
             throw IllegalQueryInput("Required")
         }
-        val number = context.request.qp("number")
+        val number = context.request.qp("number") //this is ignored right now...
         context.logger.info { "Single bill query request: '$billId'" }
         val result = try {
             SingleBillQuery(billId, context.billDAO.mainDbInst).find()
--- tools/generate_my_properties.sh
+++ tools/generate_my_properties.sh
@@ -2,9 +2,16 @@
 
 FILE_OUT=my.properties
 
-echo "CONGRESS_GIT='<get unitedstates/congress>'" > ${FILE_OUT}
+echo "DB_PATH=/Users/stew/Documents/db/congress1" > ${FILE_OUT}
+echo "CONGRESS_GIT='<get unitedstates/congress>'" >> ${FILE_OUT}
 echo "SHELL=/bin/zsh" >> ${FILE_OUT}
 echo "LEGISLATOR_GIT='<us/congress-legislators>" >> ${FILE_OUT}
-echo "DBNAME='<db name>' >> ${FILE_OUT}
+
+echo "DBNAME='<db name>'" >> ${FILE_OUT}
+
+echo "# Optional test DB setup properties:"
+echo "#TEST_DBNAME='<db name>'" >> ${FILE_OUT}
+echo "#TEST_BILL_DB_DATA=#optional dir overrides congress_git" >> ${FILE_OUT}
+echo "#TEST_LEGISLATOR_DB_DATA=#optional dir overrides legislator db data location" >> ${FILE_OUT}
 
 echo "Default my.properties created. Place this in the project root directory. Execute scripts from tools dir from root directory."
